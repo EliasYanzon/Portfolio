b520943c71bebb6bd0b3e50fb1d295c5d9e9faed
Elias Yanzon <eliasmotog3@gmail.com>
2023-06-05 18:51:23 -0300

    style of about me

--- my-portfolio/src/components/AboutMe/AboutMe.jsx
+++ my-portfolio/src/components/AboutMe/AboutMe.jsx
@@ -8,12 +8,11 @@ const AboutMe = () => {
         <img
           src={image}
           alt="Imagen de perfil"
-          className="mx-auto md:ml-0 md:mr-auto h-auto w-40 md:w-full rounded-full"
+          className="mx-auto md:ml-0 md:mr-auto md:ml-auto h-auto w-60 md:h-screen md:w-auto rounded-full"
         />
       </div>
       
-      {/* Nombre y texto */}
-      <div className="w-full md:w-1/2 flex flex-col items-center md:items-start px-4 mt-4 md:mt-0">
+      <div className="w-full md:w-1/2 flex flex-col items-center md:items-center px-4 mt-4 md:mt-0">
         <h2 className="text-2xl md:text-3xl font-bold mb-2">Elias Yanzon</h2>
         <p className="text-lg md:text-xl text-center md:text-left">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec lorem vel est ultricies semper. Duis aliquet gravida ante, ac volutpat dui volutpat vel.
